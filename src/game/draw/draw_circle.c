42bd9c397536d3b7d2fd1ab95cf234c176bc9c73
roman <mromanmail@gmail.com>
2020-10-05 22:05:38 +0300

    123

--- src/game/draw/draw_circle.c
+++ src/game/draw/draw_circle.c
@@ -6,105 +6,88 @@
 /*   By: charmon <charmon@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 22:17:28 by charmon           #+#    #+#             */
-/*   Updated: 2020/10/05 21:08:18 by charmon          ###   ########.fr       */
+/*   Updated: 2020/10/05 21:35:46 by charmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../game.h"
 
-void	texture_column(t_all all, t_img img, int column_height, const size_t texcoord, size_t win_x)
+void		texture_column(t_all all, t_img img,
+				const size_t texcoord, size_t win_x)
 {
-	int y;
-	size_t pix_x;
-	size_t pix_y;
-	size_t win_y;
+	int		y;
+	size_t	pix_x;
+	size_t	pix_y;
+	size_t	win_y;
 
 	y = 0;
-	draw_f_c(all, column_height,win_x);
-	while(y<column_height)
+	draw_f_c(all, all.col_h[win_x], win_x);
+	while (y < all.col_h[win_x])
 	{
 		pix_x = texcoord;
-		pix_y = (y*img.height)/column_height;
-		win_y = y + all.win_h/2-column_height/2;
+		pix_y = (y * img.height) / all.col_h[win_x];
+		win_y = y + all.win_h / 2 - all.col_h[win_x] / 2;
 		if ((win_y < 0 || win_y >= all.win_h) && (y += 1))
 			continue;
-		pixel_put(&all, win_x, win_y,img.data[(int)((int)pix_x + (int)pix_y *
+		pixel_put(&all, win_x, win_y, img.data[(int)((int)pix_x + (int)pix_y *
 			(img.width))]);
 		y++;
 	}
 }
 
-void	sprite_column(t_all all, t_img img, int column_height, const size_t texcoord, size_t win_x)
+void		sprite_column(t_all all, int column_height,
+					const size_t texcoord, size_t win_x)
 {
-	size_t y;
-	size_t pix_x;
-	size_t pix_y;
-	size_t win_y;
+	size_t	y;
+	size_t	pix_x;
+	size_t	pix_y;
+	size_t	win_y;
 	int		color;
 
 	y = 0;
-	while(y<column_height && column_height > 0)
+	while (y < column_height && column_height > 0)
 	{
 		pix_x = texcoord;
-		pix_y = (y*img.height)/column_height;
-		win_y = y + all.win_h/2-column_height/2;
+		pix_y = (y * all.sprite.height) / column_height;
+		win_y = y + all.win_h / 2 - column_height / 2;
 		if ((win_y < 0 || win_y >= all.win_h) && (y += 1))
 			continue;
-		color = img.data[(int)((int)pix_x + (int)pix_y * (img.width))];
+		color = all.sprite.data[(int)((int)pix_x +
+					(int)pix_y * (all.sprite.width))];
 		if (color != 9961608)
-			pixel_put(&all, win_x , win_y + 0.2 *column_height, color);
+			pixel_put(&all, win_x, win_y + 0.2 * column_height, color);
 		y++;
 	}
 }
 
-void	draw_textures(t_all *all)
+void		draw_textures(t_all *all)
 {
-	float agile;
-	size_t i;
-	t_player plr;
-	char texture;
-	float textcord;
-	float dist;
+	float		agile;
+	size_t		i;
+	char		texture;
+	float		textcord;
+	float		dist;
 
 	i = 0;
-	plr = all->player;
-	all->last_t='W';
+	all->last_t = 'W';
 	while (i < all->win_w)
 	{
-		agile = plr.dir - plr.fov/2 + plr.fov * i / (float)all->win_w;
-		all->col_h[i] = calc_text_distance(*all, agile, &texture, &textcord, &dist);
+		agile = all->player.dir - all->player.fov / 2 +
+			all->player.fov * i / (float)all->win_w;
+		all->col_h[i] = calc_text_distance(*all, agile,
+						&texture, &textcord, &dist);
 		all->last_t = texture;
-		texture_column(*all, all->textures[texture], all->col_h[i], textcord, i);
+		texture_column(*all, all->textures[texture],
+						textcord, i);
 		all->col_h[i] = dist;
 		i++;
 	}
 }
 
-void	draw_sprites(t_all all)
+void		draw_circle(t_all all)
 {
-	float agile;
-	size_t i;
-	t_player plr;
-	int column_height;
-	float textcord;
-
-	i = 0;
-	plr = all.player;
-	while (i < all.win_w)
-	{
-		agile = plr.dir - plr.fov/2 + plr.fov * i / (float)all.win_w;
-		column_height = calc_sprite_distance(all, agile, &textcord);
-		if (column_height > 0)
-			sprite_column(all, all.sprite, column_height, textcord, i);
-		i++;
-	}
-}
-
-
-void	draw_circle(t_all all)
-{
-		draw_textures(&all);
-		update_sprite_distance(all);
-		if (!all.save)
-			mlx_put_image_to_window(all.win->mlx, all.win->win, all.win->img, 0, 0);
+	draw_textures(&all);
+	update_sprite_distance(all);
+	if (!all.save)
+		mlx_put_image_to_window(all.win->mlx, all.win->win, all.win->img, 0, 0);
 }
--- src/game/draw/sprite_utils.c
+++ src/game/draw/sprite_utils.c
@@ -6,58 +6,74 @@
 /*   By: charmon <charmon@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 22:16:52 by charmon           #+#    #+#             */
-/*   Updated: 2020/09/20 10:43:55 by charmon          ###   ########.fr       */
+/*   Updated: 2020/10/05 22:04:53 by charmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../game.h"
 
-void draw_sprite(t_sprites sprite, t_all all)
+void		draw_sprite(t_sprites sprite, t_all all)
 {
-	float s_dir;
+	float	sprite_dir;
+	float	sprite_dist;
+	int		h_offset;
+	int		i;
+	size_t	sprite_screen_size;
 
-	float sprite_dir = atan2(sprite.y - all.player.y, sprite.x - all.player.x);
-	while (sprite_dir - all.player.dir >  M_PI) sprite_dir -= 2*M_PI;
-	while (sprite_dir - all.player.dir < -M_PI) sprite_dir += 2*M_PI;
-	float sprite_dist = sqrt(pow(all.player.x - sprite.x, 2) + pow(all.player.y - sprite.y, 2));
-	size_t sprite_screen_size =(int)(all.win_h/sprite_dist);
-	int h_offset = (sprite_dir - all.player.dir)*(all.win_w)/(all.player.fov) + (all.win_w)/2 - sprite_screen_size/2;
-	for (size_t i=0; i<sprite_screen_size; i++) {
-		if (h_offset+(int)i<0 || h_offset+i>=all.win_w) continue;
-		if (all.col_h[h_offset+(int)i] > sprite_dist)
-//			sprite_column(all, all.sprite,all.win_h/(sprite_dist*cos(s_dir - all.player.dir)),(i/(float)sprite_screen_size)*all.sprite.width, (size_t)h_offset+i);
-			sprite_column(all, all.sprite,sprite_screen_size,(i/(float)sprite_screen_size)*all.sprite.width, (size_t)h_offset+i);
+	sprite_dir = atan2(sprite.y - all.player.y, sprite.x - all.player.x);
+	while (sprite_dir - all.player.dir > M_PI)
+		sprite_dir -= 2 * M_PI;
+	while (sprite_dir - all.player.dir < -M_PI)
+		sprite_dir += 2 * M_PI;
+	sprite_dist = sqrt(pow(all.player.x - sprite.x, 2)
+			+ pow(all.player.y - sprite.y, 2));
+	sprite_screen_size = (int)(all.win_h / sprite_dist);
+	h_offset = (sprite_dir - all.player.dir) * (all.win_w) / (all.player.fov)
+		+ (all.win_w) / 2 - sprite_screen_size / 2;
+	i = -1;
+	while (++i < sprite_screen_size)
+	{
+		if (h_offset + i < 0 || h_offset + i >= all.win_w)
+			continue;
+		if (all.col_h[h_offset + i] > sprite_dist)
+			sprite_column(all, sprite_screen_size,
+		(i / (float)sprite_screen_size) * all.sprite.width, h_offset + i);
 	}
 }
 
-void update_sprite_distance(t_all all)
+void		calculate_distances(t_all *all)
 {
-	int i;
-	int k;
-	float dist;
-	t_player plr;
+	int		i;
 
 	i = -1;
-	plr = all.player;
-	while (++i < all.sprite_c)
-		all.sprites[i].dist = sqrt(pow(plr.x - all.sprites[i].x, 2)
-								   + pow(plr.y - all.sprites[i].y, 2));
-	k=0;
+	while (++i < all->sprite_c)
+		all->sprites[i].dist = sqrt(pow(all->player.x - all->sprites[i].x, 2)
+			+ pow(all->player.y - all->sprites[i].y, 2));
+}
+
+void		update_sprite_distance(t_all all)
+{
+	int			k;
+	float		dist;
+	int			i;
+
+	calculate_distances(&all);
+	k = 0;
 	while (k != -1)
 	{
 		i = -1;
-		k=-1;
+		k = -1;
 		dist = -1;
 		while (++i < all.sprite_c)
 			if (all.sprites[i].dist > dist)
 			{
-				k=i;
+				k = i;
 				dist = all.sprites[i].dist;
 			}
-		if (k !=-1)
+		if (k != -1)
 		{
 			draw_sprite(all.sprites[k], all);
-			all.sprites[k].dist=-1;
+			all.sprites[k].dist = -1;
 		}
 	}
 }
